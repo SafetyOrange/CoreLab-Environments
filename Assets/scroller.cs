91d9f2a5ed9d1d46589841f82e5a760392ea1538
Ben Norskov <ben@playistheantidote.com>
2015-03-22 13:59:29 -0400

    Scrolling Background

---
+++ Assets/scroller.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scroller : MonoBehaviour {
+
+	public float scrollspeed = .001f;
+	private float yOffset = 0;
+
+	private Renderer renderR;
+	// Use this for initialization
+	void Start () {
+		renderR = GetComponent<MeshRenderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		renderR.material.SetTextureOffset ("_MainTex", new Vector2 (0, yOffset)); 
+		yOffset += scrollspeed;
+	}
+}
