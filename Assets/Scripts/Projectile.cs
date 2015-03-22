32797fbf87e0ebaeaa9b4a86393d959347918305
K. Anthony Marefat <marefatk@gmail.com>
2015-03-22 16:49:20 -0400

    Projectile Boilerplate

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour {
+
+	float speed = 5;
+	Vector2 velocity = -Vector2.up;
+
+	void Start () {
+		Vector2 velocity = -Vector2.up * speed;
+	}
+
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D coll){
+		if(coll.gameObject.tag == "Player"){
+			coll.gameObject.SendMessage("hit");
+			Destroy(this.gameObject);
+		}
+
+	}
+}
