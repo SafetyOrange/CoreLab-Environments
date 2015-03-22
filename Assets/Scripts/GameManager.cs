500f440f020b3bfedfa65af67f16019e8f31e165
Ben Norskov <ben@playistheantidote.com>
2015-03-22 17:47:10 -0400

    Game Manager Working!

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,16 +1,59 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public static class GameManager {
 
+	// Variables for File Input
+	public static bool isLoaded = false;
+
+
+	//Game Variables
+	private static string[] variableNames = {
+//IMPORTANT You have to add in the name of the variable here in order for the program to find it.
+//You also have to add it in the switch statement
+		"backgroundScrollSpeed",
+		"shipSpeed"
+	};
 	public static Vector2 backgroundScrollSpeed;
 	public static Vector2 shipSpeed;
 
-	public static bool isLoaded = false;
-
 	public static void init () {
-		backgroundScrollSpeed = new Vector2 (0, .001f);
-		shipSpeed = new Vector2 (0, 0);
+		List<string> fileSplit = new List<string>();
+
+		TextAsset file = Resources.Load("GameValues") as TextAsset;
+
+		string[] s = file.text.Split (',');
+
+		foreach (string temp in s) { // Add in the 
+			if (temp.IndexOf ('\n') > 0) {
+				string[] sSplit = temp.Split ('\n');
+				if (sSplit [0] != "") { //don't add the blank line if there is one
+					fileSplit.Add (sSplit [0]);
+				}
+				if (sSplit [1] != "") { //don't add the blank line if there is one
+					fileSplit.Add (sSplit [1]);
+				}
+			} else {
+				fileSplit.Add (temp);
+			}
+		}
+		/*foreach (string theS in fileSplit) {
+			Debug.Log (theS + " fileSplit value (40)");
+		}*/
+		foreach (string key in variableNames) { //this is the line that assigns all values. If something is messing up, it's probably here. 
+			int index = fileSplit.IndexOf (key); //find index of a variable name
+			switch (key) {
+			case "backgroundScrollSpeed":
+				backgroundScrollSpeed = new Vector2 ( float.Parse(fileSplit [index + 1]), float.Parse(fileSplit [index + 2]) );
+				break;
+			case "shipSpeed":
+				shipSpeed = new Vector2 ( float.Parse(fileSplit [index + 1]), float.Parse(fileSplit [index + 2]) );
+				break;
+			}
+		}
+
+		//this should be the last name of the file
 		isLoaded = true;
 	}
 
--- Assets/Scripts/ShipMovement.cs
+++ Assets/Scripts/ShipMovement.cs
@@ -8,8 +8,18 @@ public class ShipMovement : MonoBehaviour {
 	bool firing = false;
 	
 	void Start () {
+		StartCoroutine ("loadValues");
 	}
 
+	IEnumerator loadValues() {
+		while (GameManager.isLoaded == false) {
+			Debug.Log ("not loaded");
+			yield return null;
+		}
+		xSpeed = GameManager.shipSpeed.x;
+		ySpeed = GameManager.shipSpeed.y;
+		Debug.Log (xSpeed);
+	}
 
 	void Update () {
 		checkInput();
@@ -20,11 +30,11 @@ public class ShipMovement : MonoBehaviour {
 
 		//Check Movement
 		if( Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow) ){
-			transform.position = new Vector2(transform.position.x + xSpeed, transform.position.y);
+			transform.position = new Vector3(transform.position.x - xSpeed, transform.position.y, transform.position.z);
 		}
 
 		else if( Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow) ){
-			transform.position = new Vector2(transform.position.x - xSpeed, transform.position.y);
+			transform.position = new Vector3(transform.position.x + xSpeed, transform.position.y, transform.position.z);
 		}
 
 		//Check for fire
