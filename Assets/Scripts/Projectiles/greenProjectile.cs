5da15104b45c61425794c33d806a53b1540a794f
K Anthony Marefat <marefatk@gmail.com>
2015-03-30 18:33:52 -0400

    In Class pt 2

--- Assets/Scripts/Projectiles/greenProjectile.cs
+++ Assets/Scripts/Projectiles/greenProjectile.cs
@@ -6,12 +6,12 @@ public class greenProjectile : Projectile {
 		while (!GameManager.isLoaded) {
 			yield return null;
 		}
-		velocity = GameManager.redProjectileVelocity + new Vector2( 
+		velocity = GameManager.greenProjectileVelocity + new Vector2( 
 			Random.Range(-GameManager.greenProjectileVariance.x, GameManager.greenProjectileVariance.x), 
 			Random.Range(-GameManager.greenProjectileVariance.y, GameManager.greenProjectileVariance.y)
 		);
-		glowSpeed = GameManager.redProjectileGlowSpeed;
-		damage = GameManager.redProjectileDamage;
+		glowSpeed = GameManager.greenProjectileGlowSpeed;
+		damage = GameManager.greenProjectileDamage;
 		//shotByEnemy = GameManager.redProjectileShotByEnemy;
 		loadedValues = true;
 	}
